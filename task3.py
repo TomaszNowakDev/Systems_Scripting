162212778be21969cfa234ee072a7cf870bc8484
Tomasz Nowak <tomeknew1@gmail.com>
2022-06-14 22:29:52 +0100

    Function converge() implemented, info for the user printed to the screen.

--- task3.py
+++ task3.py
@@ -18,9 +18,30 @@ def get_number():
     return number
 
 
+# Function definition
+def converge(num):
+    # print the number passed to the function cast it to an integer for correct formatting
+    print(int(num))
+    # define base case for recursive function
+    if num == 1:
+        return 1
+    # if number passed to the function is even return recursive call function with parameter number divided by 2
+    elif num % 2 == 0:
+        return converge(num/2)
+    # if number passed to the function is odd return recursive call function with parameter multiply by 3 and add 1
+    else:
+        return converge((num*3)+1)
+
+
 def main():
     # Calling function get_number() and storing result of it in number
     number = get_number()
+    print(f"You entered a number {number}.\nLet's start the game...")
+    # call the function converge() with 1 parameter and store the result of it
+    result = converge(number)
+    # if result present that means the game ended
+    if result:
+        print("Game ended!")
 
 
 if __name__ == '__main__':
